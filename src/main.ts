4fd72cffb153c46551866b18fca53ee29938fe6f
Oleksandr Honcharenko <oleksandr.honcharenko@litslink.com>
2022-07-23 15:17:42 +0300

    feat: auth finished

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+import * as passport from 'passport';
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.use(
+    session({
+      secret: 'keyboard',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  await app.listen(port, () =>
+    console.log('Server has been started at PORT - ' + port),
+  );
 }
 bootstrap();
